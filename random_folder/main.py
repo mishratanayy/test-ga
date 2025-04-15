b61f74127dbab213e78c4130f40a7d1ac1c05013
Your Name <you@example.com>
2025-04-15 22:23:19 +0530

    Testing yapf formatter

---
+++ random_folder/main.py
@@ -0,0 +1,7 @@
+import os
+
+def check_name_if_its_too_long_to_have(random_variable,another_random_variable):
+    print(random_variable,another_random_variable)
+
+if __name__ == "__main__":
+    print("Hello World")  
