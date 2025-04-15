0624b4dfaaaf3cae612d14c7d196be5cedbfbda8
Your Name <you@example.com>
2025-04-15 22:59:06 +0530

    Formatted files

--- random_folder/main.py
+++ random_folder/main.py
@@ -1,7 +1,10 @@
 import os
 
-def check_name_if_its_too_long_to_have(random_variable,another_random_variable):
-    print(random_variable,another_random_variable)
+
+def check_name_if_its_too_long_to_have(random_variable,
+                                       another_random_variable):
+    print(random_variable, another_random_variable)
+
 
 if __name__ == "__main__":
-    print("Hello World")  
+    print("Hello World")
